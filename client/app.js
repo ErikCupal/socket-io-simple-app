4da389125785e12e198867e6d08952f673e23d32
ErikCupal <erik@cup.al>
2017-02-25 13:07:51 +0100

    Initial commit

---
+++ client/app.js
@@ -0,0 +1,43 @@
+const socket = io('http://localhost:3000')
+
+let name = undefined
+
+const nameInput = document.getElementById('name-input')
+const registerButton = document.getElementById('register-button')
+
+const messageInput = document.getElementById('message-input')
+const sendButton = document.getElementById('send-button')
+
+const chat = document.getElementById('chat')
+
+socket.on('connect', () => {
+
+  registerButton.addEventListener('click', () => {
+    name = nameInput.value
+    socket.emit('REGISTER', name)
+  })
+
+  socket.on('NAME_TAKEN', () => alert('🐵 Name taken'))
+  socket.on('REGISTERED', () => {
+    alert('🎉 Registered')
+
+    sendButton.addEventListener('click', () => {
+      const message = {
+        name: name,
+        content: messageInput.value
+      }
+
+      socket.emit('CHAT_MESSAGE', message)
+      chat.innerHTML += `<li>🐵 You: ${messageInput.value}</li>`
+
+    })
+  })
+
+
+  socket.on('CHAT_MESSAGE', message => {
+    const name = message.name
+    const content = message.content
+
+    chat.innerHTML += `<li>💌 ${name}: ${content}</li>`
+  })
+})
---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const log = console.log
+
+const app = express()
+const port = 3000
+const server = http.createServer(app)
+const io = socketIo(server)
+
+server.listen(port, () => log(`🍹 Http server with Express and Socket.IO is listening on port ${port}`))
+
+// Configure basic routes
+
+app.get('/', (req, res) => {
+  res.send(
+    `
+    <h1>Hello World!</h1>
+    <br/>
+    <a href="/chat">Chat</a>
+    `
+  )
+})
+
+app.get('/chat', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+app.get('/app.js', (req, res) => {
+  res.sendFile(process.cwd() + '/client/app.js')
+})
+
+let clientNames = []
+
+io.on('connect', socket => {
+
+  log(`🍔 New socket with id ${socket.id}`)
+
+  socket.on('REGISTER', name => {
+    const possiblyExistingName = clientNames.find(clientName => clientName === name)
+
+    if (possiblyExistingName) {
+      socket.emit('NAME_TAKEN')
+    } else {
+      clientNames.push(name)
+      socket.emit('REGISTERED')
+
+      socket.on('CHAT_MESSAGE', message => {
+        socket.broadcast.emit('CHAT_MESSAGE', message)
+      })
+    }
+
+  })
+
+})
